9bb36e6e94dbbc678b0d139201817d94608fe79c
Rajshekhar jana <janarajshekhar79@gmail.com>
2025-01-04 20:26:05 +0530

    now three function add

--- string.c
+++ string.c
@@ -1,9 +1,45 @@
 #include<stdio.h>
+#include<string.h>
+void strcat_function();
+void strlen_function();
+void strcmp_function();
 int main()
 {
-    char str[10]={'B','H','O','P','A','L'};
-    int i;
-    for(i=0;str[i];i++);
-    printf("%d",i);
+    strcat_function();
+    strlen_function();
+    strcmp_function();
     return 0;
+}
+void strcat_function()
+{
+    char dest[50]="This is an";
+    char stc[50]=" example";
+    printf("\n dest befour : %s",dest);
+    strcat(dest,stc);
+    printf("\n dest after : %s",dest);
+}
+void strlen_function()
+{
+    char str[]="Midnapore College";
+    int length;
+    length=strlen(str);
+    printf("\n string : %s",str);
+    printf("\n length : %d",length);
+}
+void strcmp_function()
+{
+    char str1[]="Hello";
+    char str2[]="World";
+    char str3[]="Hello";
+    int result_1,result_2,result_3;
+    result_1=strcmp(str1,str2);
+    result_2=strcmp(str2,str3);
+    result_3=strcmp(str1,str3);
+    /*  -1 :- str1 cames before str2 in dictianary
+        1  :- str2 cames after str3 in dictianary
+        0  :- str1 and str 3 is same
+    */
+    printf("\n comperation of str1 and str2 : %d",result_1);
+    printf("\n comperation of str2 and str3 : %d",result_2);
+    printf("\n comperation of str1 and str3 : %d",result_3);
 }
