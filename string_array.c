8df5800395db4ea0617c09a468dbf2657fe21a93
Rajshekhar jana <janarajshekhar79@gmail.com>
2025-01-05 14:02:19 +0530

    add now function

--- string_array.c
+++ string_array.c
@@ -1,10 +1,26 @@
 #include<stdio.h>
+#include<string.h>
+void find_the_revers_of_string();
 int main()
 {
-    char str[20];
-    int i;
-    // for(i=0;str[i];i++);
-    fgets(str,20,stdin);
-    printf("%s",str);
+    find_the_revers_of_string();
     return 0;
+}
+void find_the_revers_of_string()
+{
+    int i,j,len;
+    char str_1[40],str_2[40];
+    printf("Enter a string: ");
+    fgets(str_1,sizeof(str_1),stdin);
+    len=strlen(str_1);
+    if (str_1[len - 1] == '\n') {
+        str_1[len - 1] = '\0';
+        len--;
+    }
+    for (i = 0, j = len - 1; j >= 0; i++, j--){
+    str_2[i]=str_1[j];
+    }
+    str_2[i]='\0';
+    printf("\n original string : %s",str_1);
+    printf("\n reverse string : %s",str_2);
 }
